dc5bf34187afa0c2fc7da2455508bbdf06804db5
Calie Rushton <calielrushton@gmail.com>
2019-01-02 11:05:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+
+
+def prime?(num)
+  arr = (2...num).to_a
+  res = arr.select { |x| num % x == 0 }
+  if res.length > 0 || num < 2
+      false
+  else
+    true
+  end
+end
+
+ # prime?(11)
