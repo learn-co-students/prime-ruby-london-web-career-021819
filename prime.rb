12deb9274084d14ee724afab45073ac0fc9794bc
Moses Olawoyin <m.bastford@gmail.com>
2019-01-23 18:36:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime?(int)
+  if int < 0 || int == 0 || int == 1
+    return false
+    
+  else
+    array_int = (2..int -1).to_a
+    array_int.all? do |num|
+     int%num > 0
+  end
+    
+  end
+    
+end
