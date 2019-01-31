90f942dd1d0e6ceafdbd6af18030d94a3496cbae
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-01-31 21:45:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(int)
+
+if int <= 1
+  return false
+end
+  
+  n = 2
+  
+  while n < int
+  
+    return false if int % n == 0
+    
+    n += 1
+    
+  end
+  
+ return true
+  
+end  
